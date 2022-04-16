99da2576b327a3aa14559c5d22a1d3064b676adb
nishchhal <nishchhal@amniltech.com>
2022-04-16 09:12:13 +0545

    Add project files.

---
+++ DependencyInjectionSamples.ConsoleApp/Program.cs
@@ -0,0 +1,6 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Microsoft.Extensions.DependencyInjection;
+
+Console.WriteLine("Hello, World!");
+var services = new ServiceCollection();
+var serviceProvider = services.BuildServiceProvider();
---
+++ DependencyInjectionSamples.SharedLib/Infrastructure/NotificationService.cs
@@ -0,0 +1,13 @@
+﻿using DependencyInjectionSamples.SharedLib.Core;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DependencyInjectionSamples.SharedLib.Infrastructure
+{
+    public class NotificationService : INotificationService
+    {
+    }
+}
