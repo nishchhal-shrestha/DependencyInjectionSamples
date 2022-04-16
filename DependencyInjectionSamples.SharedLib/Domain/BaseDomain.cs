e170df43c6d5ba0f3e6ab284ee7ede77e6d07bf9
nishchhal <nishchhal@amniltech.com>
2022-04-16 09:23:19 +0545

    adds domain and dto

---
+++ DependencyInjectionSamples.SharedLib/Domain/BaseDomain.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DependencyInjectionSamples.SharedLib.Domain
+{
+    public class BaseDomain<T>
+    {
+        public T Id { get; set; }
+    }
+}
